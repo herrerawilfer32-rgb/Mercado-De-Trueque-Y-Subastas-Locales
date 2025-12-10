ef0105b4e334d2d3c22544eccb0771c1456dad42
BBDavidSS <brayand.santosa09@gmail.com>
2025-12-10 09:07:30 -0500

    doc TipoPublicacion.java

--- proyectoFinalPOO/src/util/TipoPublicacion.java
+++ proyectoFinalPOO/src/util/TipoPublicacion.java
@@ -1,12 +1,20 @@
-/*
+/**
  * Clase: TipoPublicacion
- * Autores: Anggel Leal, Wilfer Herrera, David Santos
- * DescripciÃ³n: Clase utilidad.
- */
+ * Define los tipos de publicaciones disponibles en la plataforma.
+ * @author Anggel Leal, Wilfer Herrera, David Santos
+ * @version 1.0
+ */
 
 package util;
 
 public enum TipoPublicacion {
+	/**
+     * Publicación en modalidad de subasta.
+	 */
 	SUBASTA,
+	
+	/**
+     * Publicación en modalidad de trueque.
+	 */
 	TRUEQUE
 }
